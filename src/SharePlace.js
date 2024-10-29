3685de6a5e590842db719d9f40491c080dc1d4ae
rpamgi <rpa@mgi.org>
2024-10-29 12:28:53 +0600

    event listner added

---
+++ src/SharePlace.js
@@ -0,0 +1,12 @@
+class PlaceFinder {
+  constructor() {
+    const adressForm = document.querySelector("form");
+    const locateUserBtn = document.getElementById("locate-btn");
+
+    locateUserBtn.addEventListener("click", this.locateUserHandler);
+    adressForm.addEventListener("submit", this.findAdressHandler);
+  }
+
+  locateUserHandler() {}
+  findAdressHandler() {}
+}
